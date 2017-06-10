070f4190007d5d1e313a044da2a3d5677c5e08f9
bugaosuni59 <598586484@qq.com>
2017-06-11 01:08:54 +0800

    0

---
+++ 13-16/htdocs/regist.php
@@ -0,0 +1,54 @@
+<html>
+<body>
+
+<?php
+error_reporting(E_ALL^E_NOTICE^E_WARNING);
+
+$name = $_POST["name"];
+$pswd = $_POST["pswd"];
+
+if(strlen($pswd)<6||(!(ctype_digit($studentid)))){
+    echo "<script>alert('密码至少要六位，且为纯数字！'); history.go(-1);</script>";  	
+}
+
+if(strlen($name)<2||(!preg_match('/^\w+$/i', $nickname)))  
+{  
+    echo "<script>alert('用户名至少要两位，且不应含有非法字符！'); history.go(-1);</script>";  
+}  
+// 重复性检测
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "hmwk";
+
+$conn = new mysqli($servername,$username,$password,$dbname);
+//$conn = mysql_connect($servername,$username,$password);
+//mysql_select_db('hmwk');
+//mysql_query('set names utf8_bin');
+
+// 检测连接
+//if ($conn->connect_error) {
+//  die("Connection failed: " . $conn->connect_error);
+//}
+//echo "Connected successfully";
+
+// 检验$_POST["name"] 和$_POST["pswd"]是否存在、匹配
+$sql ="SELECT * FROM user WHERE name='".$name."';";
+//echo $sql;
+//$result=$conn->query($sql);
+//$isok=$result->num_rows();
+$result = $conn->query($sql);
+$row = $result->num_rows;
+
+if($row){	
+    echo "<script>alert('用户名已被注册！'); history.go(-1);</script>";  
+}else{
+	echo "<script>alert('登陆信息有误！请重新填写');window.location.href='index.html'</script>"; 
+}
+//echo $sql;
+	
+?>
+
+ </body>
+ </html> 
